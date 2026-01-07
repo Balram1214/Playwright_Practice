f5b107c36d68a1b1908a22c8b907b8ab684b2c87
Balram Rajale <rajalebalram@gmail.com>
2026-01-07 14:38:34 +0530

    Fixed codegen issue

--- tests/codegen.spec.js
+++ tests/codegen.spec.js
@@ -1,19 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('first codegen test', async ({ page }) => {
+test('test', async ({ page }) => {
     await page.goto('https://practicetestautomation.com/practice-test-login/');
     await page.waitForTimeout(4000);
 
     await page.getByRole('textbox', { name: 'Username' }).fill('student');
-    await page.waitForTimeout(4000);
-
     await page.getByRole('textbox', { name: 'Password' }).fill('Password123');
-
     await page.getByRole('button', { name: 'Submit' }).click();
+    await page.goto('https://practicetestautomation.com/logged-in-successfully/');
     await page.waitForTimeout(4000);
-
-    const username =await page.locator("//p[@class= 'oxd-userdropdown-name']").textContent();
-    await expect(await page.getByText(username)).toBeVisible();
-
-
 });
