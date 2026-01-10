75970f080b5efcf36d1e2cc0b7cda4618b9ab8ed
Balram Rajale <rajalebalram@gmail.com>
2026-01-10 08:36:47 +0530

    Add locator for login

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.waitForTimeout(5000);
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  await page.waitForTimeout(4000);
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-  await page.waitForTimeout(4000);
+  await page.waitForTimeout(2000);
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-  await page.waitForTimeout(4000);
+  await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.waitForTimeout(4000);
+  await page.waitForTimeout(2000);
 
-    const username =await page.locator("//p[text()='Doaa Taman']").textContent();
+    const username =await page.locator("//p[text()='Randy Franklin Gilbert']").textContent();
     await expect(page.getByText(username)).toBeVisible();
 
 });
